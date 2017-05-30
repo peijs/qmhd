a2800f1e9f7b8bc9b38fcd3ced3035d92132b02c
peijingshan <peijs@qmhd.com.cn>
2017-05-30 11:06:39 +0800

    整理！

--- 日志管理/Windows/Windows Server 2008R2/UnicomSMSLogDel.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-forfiles /p "E:\Softwareinstalllist\poweru\tomcat\logs" /s /m *.log.* /d -9 /c "cmd /c del @path
-forfiles /p "E:\Softwareinstalllist\poweru\SMGW\logs" /s /m *.log.* /d -9 /c "cmd /c del @path
